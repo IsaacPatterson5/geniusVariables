f76f8a4c0e684b545ecd5e7832b01f47cdca2d61
Isaac P <85522516+IsaacPatterson5@users.noreply.github.com>
2021-11-13 19:03:19 +0000

    Added Genius Variables

---
+++ Main.java
@@ -0,0 +1,10 @@
+class Main {
+  public static void main(String[] args) {
+    String name = "Isaac Patterson";
+	    String timeManagement = "make myself a scedule and hold myself accountable if I don't follow the scedule";
+	    String personalCommitment = "Not fall behind on my office hours and stay on track on my work";
+	    String KPIMetrics = "Growth in revenue and advertising";
+
+	    System.out.println("Hello, my name is " + name + "." + " The time management strategy that I would like to use is to " + timeManagement + "." + " My personal commitment will be to " + personalCommitment + "." + " The KPIMetrics that my business will be using are " + KPIMetrics + ".");
+	  }
+}
